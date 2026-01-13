3e5fc3e2abe4d6a45193a257a010dcf753556d6f
Jakob Bornecrantz <tbornecrantz@nvidia.com>
2026-01-13 13:38:16 +0000

    base: Introduce the base library
    Just the very basic files to get the basic things in there.

---
+++ src/xrt/base/b_stub.c
@@ -0,0 +1,14 @@
+// Copyright 2026, NVIDIA CORPORATION.
+// SPDX-License-Identifier: BSL-1.0
+/*!
+ * @file
+ * @brief  Make the compiler happy.
+ * @author Jakob Bornecrantz <tbornecrantz@nvidia.com>
+ * @ingroup base
+ */
+
+void
+b_symbol_so_that_we_are_valid_compilation_unit(void)
+{
+	// Make the compiler happy.
+}
