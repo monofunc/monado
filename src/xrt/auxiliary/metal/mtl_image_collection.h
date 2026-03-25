0c3ca6e38931c119c4e3bf22b8e5a3ee9e0b1071
Mono <81423605+monofunc@users.noreply.github.com>
2026-03-26 02:07:46 +0400

    a/metal,a/allocation: Replace IOSurface with MTLSharedTexture in allocation collection
    All textures now use MTLStorageModePrivate, unblocking depth/stencil and array textures that were incompatible with IOSurface.
    
    * Remove XRT_ALLOCATION_TYPE_IOSURFACE
    * Remove IOSurface and CoreFoundation framework linkage
    * Change storage mode from Shared to Private in create_texture_for_metal
    * Fix VkImportMetalTextureInfoEXT plane aspect for depth/stencil formats
    * Add mtl_image_collection_create_shared_from_vk for IPC server
    * Add mtl_image_collection_import_from_port for IPC client
    * Wire a_allocator with XRT_FEATURE_SERVICE compile-time selection
    * Add VK_EXT_metal_objects check in a_allocator_import_from_natives

--- src/xrt/auxiliary/metal/mtl_image_collection.h
+++ src/xrt/auxiliary/metal/mtl_image_collection.h
@@ -12,6 +12,8 @@
 #include "xrt/xrt_allocation_collection.h"
 
 
+#include <mach/mach_types.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -87,6 +89,48 @@ xrt_result_t
 mtl_image_collection_wrap_from_metal(struct vk_bundle *vk,
                                      struct xrt_allocation_collection *xac_mtl,
                                      struct xrt_allocation_collection **out_xac);
+
+/*!
+ * Like @ref mtl_image_collection_create_from_vk but creates shareable textures
+ * via [MTLDevice newSharedTextureWithDescriptor:]. Supports get_all(NATIVE_IMAGE)
+ * which exports Mach ports from MTLSharedTextureHandle.
+ *
+ * @param vk          Vulkan bundle with VK_EXT_metal_objects enabled.
+ * @param xscci       Swapchain creation info describing the images.
+ * @param image_count Number of images to allocate.
+ * @param out_xac     Pointer to receive the new allocation collection.
+ * @return XRT_SUCCESS on success.
+ *
+ * @ingroup aux_metal
+ */
+xrt_result_t
+mtl_image_collection_create_shared_from_vk(struct vk_bundle *vk,
+                                            const struct xrt_swapchain_create_info *xscci,
+                                            uint32_t image_count,
+                                            struct xrt_allocation_collection **out_xac);
+
+/*!
+ * Create an allocation collection by importing textures from Mach ports.
+ *
+ * Each port is a send right from MTLSharedTextureHandle. On success,
+ * ownership of all ports transfers to the collection (send rights are
+ * deallocated after import). On failure, caller retains ownership.
+ *
+ * @param mtl_device  MTLDevice as void*.
+ * @param xscci       Swapchain creation info.
+ * @param ports       Array of Mach port send rights.
+ * @param image_count Number of ports/images.
+ * @param out_xac     Pointer to receive the new allocation collection.
+ * @return XRT_SUCCESS on success.
+ *
+ * @ingroup aux_metal
+ */
+xrt_result_t
+mtl_image_collection_import_from_port(void *mtl_device,
+                                       const struct xrt_swapchain_create_info *xscci,
+                                       const mach_port_t *ports,
+                                       uint32_t image_count,
+                                       struct xrt_allocation_collection **out_xac);
 
 
 #ifdef __cplusplus
